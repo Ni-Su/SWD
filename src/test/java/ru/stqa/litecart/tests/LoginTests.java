409e2fa234bd3c9b539481ce22c46e9151576298
Ni-Su <nikolaysukhikh81@gmail.com>
2024-01-19 10:12:34 +0500

    Create LoginTests.java
    Create LoginTests, задание №3

---
+++ src/test/java/ru/stqa/litecart/tests/LoginTests.java
@@ -0,0 +1,35 @@
+package ru.stqa.litecart.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTests {
+    private WebDriver wd;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testOpenPage() throws Exception {
+        wd.get("http://localhost/litecart/admin/login.php");
+        wd.findElement(By.name("username")).sendKeys("admin");
+        wd.findElement(By.name("password")).sendKeys("admin");
+        wd.findElement(By.name("login")).click();
+
+    }
+
+    @AfterMethod
+    public void tearDown () {
+        wd.quit();
+    }
+
+}
