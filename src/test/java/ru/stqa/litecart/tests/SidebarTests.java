96196bcd05e719066d3ced029307c8245c65b7aa
Ni-Su <nikolaysukhikh81@gmail.com>
2024-01-24 10:42:36 +0500

    SidebarTests - проходит по всем разделам и подразделам бокового меню

--- src/test/java/ru/stqa/litecart/tests/SidebarTests.java
+++ src/test/java/ru/stqa/litecart/tests/SidebarTests.java
@@ -18,8 +18,6 @@ public class SidebarTests {
 
     @BeforeMethod
     public void start() {
-        //DesiredCapabilities caps = new DesiredCapabilities();
-        //wd = new FirefoxDriver();
         FirefoxOptions options = new FirefoxOptions();
         wd = new FirefoxDriver(options);
         options.setBinary(new FirefoxBinary(new File("c:\\Program Files\\Firefox Nightly\\firefox.exe")));
@@ -36,28 +34,72 @@ public class SidebarTests {
         wd.findElement(By.name("username")).sendKeys("admin");
         wd.findElement(By.name("password")).sendKeys("admin");
         wd.findElement(By.name("login")).click();
+
         wd.findElement(By.linkText("Appearence")).click();
         wd.findElement(By.linkText("Logotype")).click();
-        //wd.findElement(By.name("doc-logotype")).click();
-        //wd.findElement(By.cssSelector("id.doc-logotype"));
+
         wd.findElement(By.linkText("Catalog")).click();
-        //wd.findElement(By.linkText("Add New Category")).click();
-        //wd.findElement(By.name("save")).click();
+        wd.findElement(By.linkText("Product Groups")).click();
+        wd.findElement(By.linkText("Option Groups")).click();
+        wd.findElement(By.linkText("Manufacturers")).click();
+        wd.findElement(By.linkText("Suppliers")).click();
+        wd.findElement(By.linkText("Delivery Statuses")).click();
+        wd.findElement(By.linkText("Sold Out Statuses")).click();
+        wd.findElement(By.linkText("Quantity Units")).click();
+        wd.findElement(By.linkText("CSV Import/Export")).click();
+
         wd.findElement(By.linkText("Countries")).click();
+
         wd.findElement(By.linkText("Currencies")).click();
+
         wd.findElement(By.linkText("Customers")).click();
+        wd.findElement(By.linkText("CSV Import/Export")).click();
+        wd.findElement(By.linkText("Newsletter")).click();
+
         wd.findElement(By.linkText("Geo Zones")).click();
+
         wd.findElement(By.linkText("Languages")).click();
+        wd.findElement(By.linkText("Storage Encoding")).click();
+
         wd.findElement(By.linkText("Modules")).click();
+        wd.findElement(By.linkText("Customer")).click();
+        wd.findElement(By.linkText("Shipping")).click();
+        wd.findElement(By.linkText("Payment")).click();
+        wd.findElement(By.linkText("Order Total")).click();
+        wd.findElement(By.linkText("Order Success")).click();
+        wd.findElement(By.linkText("Order Action")).click();
+
         wd.findElement(By.linkText("Orders")).click();
+        wd.findElement(By.linkText("Order Statuses")).click();
+
         wd.findElement(By.linkText("Pages")).click();
+
         wd.findElement(By.linkText("Reports")).click();
+        wd.findElement(By.linkText("Most Sold Products")).click();
+        wd.findElement(By.linkText("Most Shopping Customers")).click();
+
         wd.findElement(By.linkText("Settings")).click();
+        wd.findElement(By.linkText("Defaults")).click();
+        wd.findElement(By.linkText("General")).click();
+        wd.findElement(By.linkText("Listings")).click();
+        wd.findElement(By.linkText("Images")).click();
+        wd.findElement(By.linkText("Checkout")).click();
+        wd.findElement(By.linkText("Advanced")).click();
+        wd.findElement(By.linkText("Security")).click();
+
         wd.findElement(By.linkText("Slides")).click();
+
         wd.findElement(By.linkText("Tax")).click();
+        wd.findElement(By.linkText("Tax Rates")).click();
+
         wd.findElement(By.linkText("Translations")).click();
+        wd.findElement(By.linkText("Scan Files")).click();
+        wd.findElement(By.linkText("CSV Import/Export")).click();
+
         wd.findElement(By.linkText("Users")).click();
+
         wd.findElement(By.linkText("vQmods")).click();
+
     }
     @AfterMethod
     public void stop() {
