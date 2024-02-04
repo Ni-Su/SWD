6a3457467256a396c85aa5b6c2eeae3ec39c9f91
Ni-Su <nkolaysukhikh81@gmail.com>
2024-02-04 12:48:24 +0500

    изменил локатор поиска названия станы, создал сортированую копию списка и сравнил списки, задание№8 часть 1

--- src/test/java/ru/stqa/litecart/tests/SortCountriesTests.java
+++ src/test/java/ru/stqa/litecart/tests/SortCountriesTests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -12,6 +13,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static java.util.Collections.*;
 
 public class SortCountriesTests {
     //Collections.sort(list);
@@ -28,14 +31,16 @@ public class SortCountriesTests {
         wd.findElement(By.name("password")).sendKeys("admin");
         wd.findElement(By.name("login")).click();
 
-        List<String> countries = new ArrayList<>();
-        List<WebElement> name = wd.findElements(By.cssSelector("table.dataTable a"));
+        List<String> countries = new ArrayList<String>();
+        List<WebElement> name = wd.findElements(By.cssSelector("tr.row>td:nth-child(5)"));
         for(WebElement n : name) {
             String na = n.getAttribute("innerText");
             countries.add(na);
         }
-        List<String> countriessort = new ArrayList<>();
 
+        List<String> cs = countries;
+        Collections.sort(cs);
+        Assert.assertEquals(countries, cs);
     }
     @AfterMethod
     public void tearDown () {
