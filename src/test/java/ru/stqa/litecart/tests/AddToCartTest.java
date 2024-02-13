da1f7b29f4119733954b4be56d43965c99cec9e2
Ni-Su <nikolaysukhikh81@gmail.com>
2024-02-13 22:48:28 +0500

    исправил ошибки, задание №13

--- src/test/java/ru/stqa/litecart/tests/AddToCartTest.java
+++ src/test/java/ru/stqa/litecart/tests/AddToCartTest.java
@@ -9,11 +9,12 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.NoSuchElementException;
+import org.openqa.selenium.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 public class AddToCartTest {
     private WebDriver wd;
+    private int items = 0;
 
     @BeforeMethod
     public void setUp() {
@@ -23,16 +24,22 @@ public class AddToCartTest {
 
     @Test
     public void testAddToCart(){
-        wd.get("http://localhost/litecart/en/");
-        wd.findElement(By.cssSelector("div#box-most-popular li.product:first-child")).click();
-        wd.findElement(By.cssSelector("td.options>strong"));
-        if(isElementPresent(By.cssSelector("td.options>strong"))) {
-            Select size = new Select(wd.findElement(By.cssSelector("select")));
-            size.selectByValue("Small");
+        while (items <= 3 ) {
+            wd.get("http://localhost/litecart/en/");
+            wd.findElement(By.cssSelector("div#box-most-popular li.product:first-child")).click();
+            //wd.findElement(By.cssSelector("td.options>strong"));
+            System.out.println(isElementPresent(By.cssSelector("td.options>strong")));
+            if(isElementPresent(By.cssSelector("td.options>strong"))) {
+                Select size = new Select(wd.findElement(By.cssSelector("select")));
+                size.selectByValue("Small");
+                wd.findElement(By.cssSelector("button[name=add_cart_product]")).click();
+            }else {
+                wd.findElement(By.cssSelector("button[name=add_cart_product]")).click();
+                //добавить ожидание
+            }
+            items = Integer.parseInt(wd.findElement(By.cssSelector("span.quantity")).getAttribute("innerText"));
+            System.out.println(items);
         }
-        wd.findElement(By.cssSelector("button[name=add_cart_product]")).click();
-
-
 
 
     }
