60a7861d14e04add0e7397de590403cb22c276a3
Ni-Su <nikolaysukhikh81@gmail.com>
2024-02-19 07:05:00 +0500

    Проверка открытия ссылок в новом окне, задание №14

--- src/test/java/ru/stqa/litecart/tests/NewWindowTests.java
+++ src/test/java/ru/stqa/litecart/tests/NewWindowTests.java
@@ -2,20 +2,29 @@ package ru.stqa.litecart.tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class NewWindowTests {
     private WebDriver wd;
+    private WebDriverWait wait;
 
     @BeforeMethod
     public void setUp() {
         wd = new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        //wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        wait = new WebDriverWait(wd, Duration.ofSeconds(5));
     }
     @Test
     public void testNewWindow(){
@@ -26,9 +35,29 @@ public class NewWindowTests {
         //wd.get("http://localhost/litecart/admin/?app=countries&doc=countries");
         wd.findElement(By.linkText("Countries")).click();
         wd.findElement(By.linkText("Afghanistan")).click();
+        List<WebElement> links = wd.findElements(By.cssSelector("tbody td tr a[href^=http][target]"));
+
+        for(WebElement link : links) {
+            String orginalWindow = wd.getWindowHandle();
+            Set<String> existingWindows = wd.getWindowHandles();
+            link.click();
+            String newWindow = wait.until(anyWindowOtherThan(existingWindows));
+            wd.switchTo().window(newWindow);
+            wd.close();
+            wd.switchTo().window(orginalWindow);
+
+        }
 
     }
-
+    public ExpectedCondition<String> anyWindowOtherThan(Set<String> oldWindows) {
+        return new ExpectedCondition<String>() {
+            public String apply(WebDriver wd) {
+                Set<String> habdles = wd.getWindowHandles();
+                habdles.removeAll(oldWindows);
+                return habdles.size() > 0 ? habdles.iterator().next():null;
+            }
+        };
+    }
     @AfterMethod
     public void tearDown() { wd.quit(); }
 }
