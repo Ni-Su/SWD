d0a20b0a28388987d989aa5afb0b4d14b2e60d7e
Ni-Su <nikolaysukhikh81@gmail.com>
2024-02-20 10:05:38 +0500

    в логе браузера выводим сообщения любого уровня, задание №17

--- src/test/java/ru/stqa/litecart/tests/LogsTests.java
+++ src/test/java/ru/stqa/litecart/tests/LogsTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.litecart.tests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.InvalidSelectorException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,6 +15,7 @@ import java.util.concurrent.TimeUnit;
 
 public class LogsTests {
     private WebDriver wd;
+    private int i = 0;
 
     @BeforeMethod
     public void setUp() {
@@ -27,18 +29,40 @@ public class LogsTests {
         wd.findElement(By.name("password")).sendKeys("admin");
         wd.findElement(By.name("login")).click();
         wd.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1");
+        List<LogEntry> m = wd.manage().logs().get("browser").getAll();
+        System.out.println(m);
+
         wd.findElement(By.linkText("Rubber Ducks")).click();
+        wd.manage().logs().get("browser").forEach(l -> System.out.println(l));
+
         wd.findElement(By.linkText("Subcategory")).click();
         List<WebElement> products = wd.findElements(By.cssSelector("tbody td>a[href*=product]"));
-        for (WebElement product : products) {
-            product.click();
+        while (i <= products.size() -1) {
+            System.out.println(i);
+            List<WebElement> pr = wd.findElements(By.cssSelector("tbody td>a[href*=product]"));
+            pr.get(i).click();
             System.out.println(wd.manage().logs().getAvailableLogTypes());
             for(LogEntry l : wd.manage().logs().get("browser").getAll()) {
                 System.out.println(l);
+                System.out.println("browser");
             }
+            for(LogEntry l : wd.manage().logs().get("driver").getAll()) {
+                System.out.println(l);
+                System.out.println("driver");
+            }
+            for(LogEntry l : wd.manage().logs().get("client").getAll()) {
+                System.out.println(l);
+                System.out.println("client");
+            }
+            wd.get("http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1");
+            wd.findElement(By.linkText("Rubber Ducks")).click();
+            wd.findElement(By.linkText("Subcategory")).click();
+            i = i +2;
         }
 
     }
     @AfterMethod
     public void tearDown(){wd.quit();}
+
+
 }
